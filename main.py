5b91d349dda188ee4270c53fe69d1143cc2c3db7
didihearthatright <dymark@eastlink.ca>
2026-06-01 10:58:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://dihtr.com", "http://localhost:8000", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
